805e418cc286387d99f399bb0e6d53cfc2777833
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-04-08 12:43:48 -0400

    Added ingredients to the cocktail program

--- class26/objects-shots/js/main.js
+++ class26/objects-shots/js/main.js
@@ -11,7 +11,12 @@ function getDrink() {
             console.log(data.drinks[0])
             document.querySelector('h2').innerText = data.drinks[0].strDrink
             document.querySelector('img').src = data.drinks[0].strDrinkThumb
-            document.querySelector('h3').innerText = data.drinks[0].strInstructions
+            document.querySelector('#instructions').innerText = data.drinks[0].strInstructions
+            document.querySelector('#ing1').innerText = data.drinks[0].strIngredient1
+            document.querySelector('#ing2').innerText = data.drinks[0].strIngredient2
+            document.querySelector('#ing3').innerText = data.drinks[0].strIngredient3
+            document.querySelector('#ing4').innerText = data.drinks[0].strIngredient4
+            document.querySelector('#ing5').innerText = data.drinks[0].strIngredient5
         })
         .catch(err => {
             console.log(`error ${err}`);
