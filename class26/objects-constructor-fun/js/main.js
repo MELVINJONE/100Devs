6cdf140657aee66c7eefe0b5d623ec9c59c01180
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-04-10 09:47:36 -0400

    started creating the doggo generator

--- class26/objects-constructor-fun/js/main.js
+++ class26/objects-constructor-fun/js/main.js
@@ -1,22 +1,40 @@
-document.querySelector('button').addEventListener('click', getDrink)
+// document.querySelector('button').addEventListener('click', getDrink)
 
-function getDrink() {
-    let drink = document.querySelector('input').value
+// function getDrink() {
+//     let drink = document.querySelector('input').value
 
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+//     fetch(`https://http.dog/${number}.jpg`)
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log(data.number[100])
+//             document.querySelector('h2').innerText = data.drinks[0].strDrink
+//             document.querySelector('img').src = data.drinks[0].strDrinkThumb
+//             document.querySelector('#instructions').innerText = data.drinks[0].strInstructions
+//             document.querySelector('#ing1').innerText = data.drinks[0].strMeasure1 + ' ' + data.drinks[0].strIngredient1
+//             document.querySelector('#ing2').innerText = `${data.drinks[0].strMeasure2} ${data.drinks[0].strIngredient2}`
+//             document.querySelector('#ing3').innerText = `${data.drinks[0].strMeasure3} ${data.drinks[0].strIngredient3}`
+//             document.querySelector('#ing4').innerText = `${data.drinks[0].strMeasure4} ${data.drinks[0].strIngredient4}`
+//             document.querySelector('#ing5').innerText = `${data.drinks[0].strMeasure5} ${data.drinks[0].strIngredient5}`
+//         })
+//         .catch(err => {
+//             console.log(`error ${err}`);
+//         });
+// }
+
+document.querySelector('button').addEventListener('click', findDog)
+
+function findDog() {
+    let dog = document.querySelector('input').value
+
+    fetch('https://http.dog/200.jpg')
         .then(res => res.json())
         .then(data => {
-            console.log(data.drinks[0])
-            document.querySelector('h2').innerText = data.drinks[0].strDrink
-            document.querySelector('img').src = data.drinks[0].strDrinkThumb
-            document.querySelector('#instructions').innerText = data.drinks[0].strInstructions
-            document.querySelector('#ing1').innerText = data.drinks[0].strMeasure1 + ' ' + data.drinks[0].strIngredient1
-            document.querySelector('#ing2').innerText = `${data.drinks[0].strMeasure2} ${data.drinks[0].strIngredient2}`
-            document.querySelector('#ing3').innerText = `${data.drinks[0].strMeasure3} ${data.drinks[0].strIngredient3}`
-            document.querySelector('#ing4').innerText = `${data.drinks[0].strMeasure4} ${data.drinks[0].strIngredient4}`
-            document.querySelector('#ing5').innerText = `${data.drinks[0].strMeasure5} ${data.drinks[0].strIngredient5}`
+            console.log(data.message);
+            document.querySelector('img').src = data.message
         })
         .catch(err => {
-            console.log(`error ${err}`);
-        });
+            console.log(`error${err}`);
+        })
+
+
 }
--- class26/objects-dog-photos/js/main.js
+++ class26/objects-dog-photos/js/main.js
@@ -1,1 +1,25 @@
-//Get a dog photo from the dog.ceo api and place the photo in the DOM
+const BREED_URL = 'https://dog.ceo/api/breeds/list/all'
+
+const select = document.querySelector('.breeds')
+
+document.querySelector('select').addEventListener('click', showDog)
+
+function showDog() {
+    fetch(BREED_URL)
+        .then(res => {
+            return res.json()
+        })
+        .then(data => { //Grab the data
+            const breedsObject = data.message //Turn the message from data into an object
+            // console.log(breedsObject); It just serves to see it on the console (what the keys are)
+            const breedsArray = Object.keys(breedsObject) //Turn the object into an array
+            // console.log(breedsArray);
+            for (let i = 0; i < breedsArray.length; i++) {
+                const option = document.createElement('option')//Create an element in the HTML file called option so <option></option>. But it still doesn't display anything.
+                option.value = breedsArray[i]//Is adding a value to each option so <option value='breed'></option>
+                option.innerText = breedsArray[i] //Add the text inside the option element
+                select.appendChild(option) //Find the selector with class of .breeds (look at 2nd line on top) and append each option to be used with the selected tag(the dropdown box). 
+            }
+
+        })
+}
