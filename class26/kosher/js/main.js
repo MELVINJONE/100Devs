e1453b26c079f7da973a878916cae4287e8e1d50
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-04-12 11:40:32 -0400

    added ternary operator to check if any ingredient is falsy or truthy

--- class26/kosher/js/main.js
+++ class26/kosher/js/main.js
@@ -47,19 +47,19 @@ class ProductInfo {
     }
 
     listIngredients() {
-        let tableRef = document.getElementById('ingredient-table')
+        let tableRef = document.getElementById('ingredient-table') //specify what we're targeting. In this case it's our table with id ingredient-table
 
-        for (let i = 1; i < tableRef.rows.length;) { //clear rows that are below the header (so skip the 1st row). Bc otherwise it appends from the previous one.
+        for (let i = 1; i < tableRef.rows.length;) { //Quickly go through the table and if there are existing rows we delete them. Clear rows that are below the header (so skip the 1st row). Bc otherwise it appends from the previous search.
             tableRef.deleteRow(i) //whatever row we're on, delete the entire row. And loop through whole table. But you don't have to add the i++ bc each time you delete one row it moves up.
         }
         if (!(this.ingredients == null)) { //check if ingredients are not null and then enter the for loop
 
             for (let key in this.ingredients) { //For each ingredient in our list of ingredients
                 let newRow = tableRef.insertRow(-1) //inserts new row in our table at the end. To append row at the end of our table use -1, like for an array if we wanna add something at the end of it use -1.
-                let newICell = newRow.insertCell(0) //insert a cell
-                let newVCell = newRow.insertCell(1) //insert another cell
+                let newICell = newRow.insertCell(0) //insert a cell on the left (point 0 on the row)
+                let newVCell = newRow.insertCell(1) //insert another cell on the right (point 1 on the row)
                 let newIText = document.createTextNode(this.ingredients[key].text)   //Pass in some text in the space we specified. Create new text value for each ingredient
-                let vegStatus = this.ingredients[key].vegetarian
+                let vegStatus = this.ingredients[key].vegetarian ? this.ingredients[key].vegetarian : 'unknown' //if it's any kind of falsy value return unknown  else return the value
                 let newVText = document.createTextNode(vegStatus) //Create new status for the vegetarian
                 newICell.appendChild(newIText) //put text value inside cell we created
                 newVCell.appendChild(newVText) //put the text into the cell
