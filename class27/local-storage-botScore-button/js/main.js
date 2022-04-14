2e29eee05147def11e7df895509051b486b88d56
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-04-14 12:30:25 -0400

    finished the local storage exercise

--- class27/local-storage-botScore-button/js/main.js
+++ class27/local-storage-botScore-button/js/main.js
@@ -1,6 +1,6 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
 
-if (!localStorage.getItem('botScore')) { //if it's not false then run the line
+if (!localStorage.getItem('botScore')) { //if botscore is there, don't do anything. If it's not there run the line (if we don't get a value back = if it's not null or false)
     localStorage.setItem('botScore', 0)// if there's not a botscore yet set it to 0
 }
 
@@ -8,7 +8,7 @@ document.querySelector('button').addEventListener('click', addAnotherOne) //even
 
 function addAnotherOne() {
     let botScoreVal = Number(localStorage.getItem('botScore')) //get value and store to variable
-    botScoreVal += 1 //ad 1 to variable
-    localStorage.setItem('botScore', botScoreVal) //reset that score and adds 1
+    botScoreVal += 1 //when we click the button add 1 to variable
+    localStorage.setItem('botScore', botScoreVal) //reset that score to whatever it was plus 1
 
 }
