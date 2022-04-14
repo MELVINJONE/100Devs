917d162968eff74203bee85938c690c36c660e18
Julia Furst Morgado <juliafmorgado@gmail.com>
2022-04-14 16:06:45 -0400

    fixed carousel style and got the right arrow to work

--- class26/objects-shots/js/main.js
+++ class26/objects-shots/js/main.js
@@ -1,24 +1,50 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+// //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 
-document.querySelector('button').addEventListener('click', getDrink)
+// document.querySelector('button').addEventListener('click', getDrink)
 
-function getDrink() {
-    let drink = document.querySelector('input').value
+// function getDrink() {
+//     let drink = document.querySelector('input').value
 
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data.drinks[0])
-            document.querySelector('h2').innerText = data.drinks[0].strDrink
+//     fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log(data)
+//             document.querySelector('h2').innerText = data.drinks.strDrink
+//             document.querySelector('img').src = data.drinks.strDrinkThumb
+//             document.querySelector('#instructions').innerText = data.drinks.strInstructions
+//             document.querySelector('#ing1').innerText = data.strMeasure1 + ' ' + data.strIngredient1
+//             document.querySelector('#ing2').innerText = `${data.drinks[0].strMeasure2} ${data.drinks[0].strIngredient2}`
+//             document.querySelector('#ing3').innerText = `${data.drinks[0].strMeasure3} ${data.drinks[0].strIngredient3}`
+//             document.querySelector('#ing4').innerText = `${data.drinks[0].strMeasure4} ${data.drinks[0].strIngredient4}`
+//             document.querySelector('#ing5').innerText = `${data.drinks[0].strMeasure5} ${data.drinks[0].strIngredient5}`
+//         })
+//         .catch(err => {
+//             console.log(`error ${err}`);
+//         });
+// }
+
+
+
+document.querySelector('.forward').addEventListener('click', getFetch)
+
+function getFetch(){
+//   const choice = document.querySelector('input').value
+  const url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('h2').innerText = data.drinks[0].strDrink
             document.querySelector('img').src = data.drinks[0].strDrinkThumb
             document.querySelector('#instructions').innerText = data.drinks[0].strInstructions
-            document.querySelector('#ing1').innerText = data.drinks[0].strMeasure1 + ' ' + data.drinks[0].strIngredient1
+            document.querySelector('#ing1').innerText = `${data.drinks[0].strMeasure1} ${data.drinks[0].strIngredient1}`
             document.querySelector('#ing2').innerText = `${data.drinks[0].strMeasure2} ${data.drinks[0].strIngredient2}`
             document.querySelector('#ing3').innerText = `${data.drinks[0].strMeasure3} ${data.drinks[0].strIngredient3}`
             document.querySelector('#ing4').innerText = `${data.drinks[0].strMeasure4} ${data.drinks[0].strIngredient4}`
             document.querySelector('#ing5').innerText = `${data.drinks[0].strMeasure5} ${data.drinks[0].strIngredient5}`
-        })
-        .catch(err => {
-            console.log(`error ${err}`);
-        });
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
 }
